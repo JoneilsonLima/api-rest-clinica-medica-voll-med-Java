fd32b09934a5b81d4ebb9c7a883f549a0bd2dc2c
joneilson pereira <joneilson84@gmail.com>
2024-04-21 15:24:06 -0300

    feat: adicionando api para listagem de paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,13 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -23,5 +24,8 @@ public class PacienteController {
         pacienteRepository.save(new Paciente(dados));
     }
 
-
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+        return pacienteRepository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
 }
